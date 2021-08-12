42c20f807ee78e9cddfda2ea6538c8343624e7ae
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-12 15:52:08 -0700

    update tests, and add sections for division and multiplication

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 
-// import functions and grab DOM elements
-
 import { add, subtract, multiply, divide } from './utils.js';
 
 const addEl1 = document.getElementById ('add-num1');
 const addEl2 = document.getElementById ('add-num2');
 const subEl1 = document.getElementById ('sub-num1');
 const subEl2 = document.getElementById ('sub-num2');
+const multEl1 = document.getElementById ('mult-num1');
+const multEl2 = document.getElementById ('mult-num2');
+const divEl1 = document.getElementById ('div-num1');
+const divEl2 = document.getElementById ('div-num2');
 const addButton = document.getElementById ('add-button');
 const addAnswer = document.getElementById ('add-answer');
 const subButton = document.getElementById ('sub-button');
 const subAnswer = document.getElementById ('sub-answer');
-
-//
+const multAnswer = document.getElementById ('mult-answer');
+const multButton = document.getElementById ('mult-button');
+const divButton = document.getElementById ('div-button');
+const divAnswer = document.getElementById ('div-answer');
 
 addButton.addEventListener('click', () => {
     const x = Number(addEl1.value);
@@ -26,4 +30,16 @@ subButton.addEventListener('click', () => {
     const y = Number(subEl2.value);
     const result = subtract(x, y);
     subAnswer.textContent = result;
+});
+multButton.addEventListener('click', () => {
+    const x = Number(multEl1.value);
+    const y = Number(multEl2.value);
+    const result = multiply(x, y);
+    multAnswer.textContent = result;
+});
+divButton.addEventListener('click', () => {
+    const x = Number(divEl1.value);
+    const y = Number(divEl2.value);
+    const result = divide(x, y);
+    divAnswer.textContent = result;
 });
--- test.js
+++ test.js
@@ -12,3 +12,15 @@ test('sub should take in 6 and 2 and return 4', (expect) => {
     const actual = subtract(6, 2);
     expect.equal(actual, expected);
 });
+
+test('multiply should take in 5 and 10 and return 500', (expect) => {
+    const expected = 500;
+    const actual = multiply(5, 10);
+    expect.equal(actual, expected);
+});
+
+test('div should take in 210 and 3 return 70', (expect) => {
+    const expected = 70;
+    const actual = divide(210, 3);
+    expect.equal(actual, expected);
+});
