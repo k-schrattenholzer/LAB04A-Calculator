eab388f7d8e79f3f3f7536c2121bffe15167894b
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-12 15:09:32 -0700

    debugging testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,5 @@
-// IMPORT MODULES under test here:
+
 import {add} from '../utils.js' ;
-
-// import { example } from '../example.js';
-
-const test = QUnit.test;
 
 test('add should take in 5 and 2 and return 7', (expect) => {
     const expected = 7;
